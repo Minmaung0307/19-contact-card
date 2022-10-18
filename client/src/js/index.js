5c1b732d84b3a5a8a3827caef9914751b2418f3a
Min Maung <minmaung0307@gmail.com>
2022-10-18 11:37:20 -0400

    update lesson3 indexedDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,6 +3,7 @@ import "./form";
 import "./submit";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { initdb } from "./database";
 
 //Import Images
 import Logo from "../images/logo.png";
@@ -11,6 +12,7 @@ import Dog from "../images/dog.png";
 
 //Add Images on Load
 window.addEventListener("load", function () {
+  initdb();
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
